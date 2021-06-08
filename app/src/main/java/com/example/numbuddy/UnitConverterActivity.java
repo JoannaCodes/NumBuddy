b0511ef3a00b32f2f98b4b9565cecc4e2ea4e2bb
Joanna Marie <joannalara21@gmail.com>
2021-06-08 20:46:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/numbuddy/UnitConverterActivity.java
@@ -0,0 +1,14 @@
+package com.example.numbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UnitConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_converter);
+    }
+}
