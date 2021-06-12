0cd86dba66e0b17c83ed9d970f70d6184e246128
Queennie Llido <72775101+qmsllido@users.noreply.github.com>
2021-06-13 02:12:28 +0800

    Added questions for easy level
    No show answer and solution yet

---
+++ app/src/main/java/com/example/numbuddy/EasyQuestions.java
@@ -0,0 +1,14 @@
+package com.example.numbuddy;
+
+public class EasyQuestions {
+    private int qImage;
+
+    public EasyQuestions(int qImage) {
+        this.qImage = qImage;
+    }
+
+    public int getqImage() {
+        return qImage;
+    }
+}
+
---
+++ app/src/main/java/com/example/numbuddy/MiniEasyGamesActivity.java
@@ -0,0 +1,51 @@
+package com.example.numbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MiniEasyGamesActivity extends AppCompatActivity {
+    private ImageView qImageView;
+    private Button qButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mini_easy_games);
+
+        qImageView = (ImageView) findViewById(R.id.easyProbview);
+        qButton = (Button)findViewById(R.id.nxtQuest_Btn) ;
+
+
+        qButton.setOnClickListener(v -> showRandomEasyQuestion());
+
+        showRandomEasyQuestion();
+
+    }
+
+    public void showRandomEasyQuestion(){
+        shuffleEasyQuestions();
+
+        qImageView.setImageResource(questionsArray[0].getqImage());
+    }
+
+    EasyQuestions q01 = new EasyQuestions(R.drawable.problem_easy_1);
+    EasyQuestions q02 = new EasyQuestions(R.drawable.problem_easy_2);
+    EasyQuestions q03 = new EasyQuestions(R.drawable.problem_easy_3);
+    EasyQuestions q04 = new EasyQuestions(R.drawable.problem_easy_4);
+    EasyQuestions q05 = new EasyQuestions(R.drawable.problem_easy_5);
+
+    EasyQuestions[] questionsArray = new EasyQuestions[]{
+            q01, q02, q03, q04, q05
+    };
+
+    public void shuffleEasyQuestions() {
+        Collections.shuffle(Arrays.asList(questionsArray));
+    }
+}
